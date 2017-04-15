0b96b043ced80bf561fb7a697d72bad8f7a5a878
Craig Oliver <craig.robert.oliver@gmail.com>
2017-04-15 15:11:21 -0400

    WIP Remember values from drop-downs

--- php/index.php
+++ php/index.php
@@ -4,6 +4,40 @@
 </head>
 <body>
 
+<?php
+
+/* Grab quantity if specified */
+$quantity = 1;
+if (isset($_GET['quantity']) && !empty($_GET['quantity'])) {
+    $quantity = $_GET['quantity'];
+}
+
+/* Grab level if specified */
+$level = 1;
+if (isset($_GET['level']) && !empty($_GET['level'])) {
+    $level = $_GET['level'];
+}
+
+function createIntegerDropDown($name, $minValue, $maxValue, $selectedValue) {
+    echo "<select";
+    echo " name='$name'";
+    echo ">";
+    for ($value = $minValue; $value <= $maxValue; $value++) {
+        echo "<option";
+        echo " value='$value'";
+        if ($value == $selectedValue) {
+            echo " selected='selected'";
+        }
+        echo ">";
+        echo $value;
+        echo "</option>";
+    }
+    echo "</select>";
+}
+
+?>
+
+
 <h1>Your Party</h1>
 
 <p>Please choose how many characters are in your party, and what level they are:</p>
@@ -15,43 +49,8 @@
             <th>Level</th>
         </tr>
         <tr>
-            <td>
-                <select name="quantity" value="1">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                </select>
-            </td>
-            <td>
-                <select name="level" value="1">
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                    <option value="9">9</option>
-                    <option value="10">10</option>
-                    <option value="11">11</option>
-                    <option value="12">12</option>
-                    <option value="13">13</option>
-                    <option value="14">14</option>
-                    <option value="15">15</option>
-                    <option value="16">16</option>
-                    <option value="17">17</option>
-                    <option value="18">18</option>
-                    <option value="19">19</option>
-                    <option value="20">20</option>
-                </select>
-            </td>
+            <td> <?php createIntegerDropDown("quantity",1,10,$quantity); ?> </td>
+            <td> <?php createIntegerDropDown("level",1,20,$quantity); ?> </td>
         </tr>
     </table>
     <input type="submit"></input>
@@ -71,6 +70,9 @@ if (!isset($_GET['level']) || empty($_GET['level'])) {
 
 ?>
 
-    <p>OK, you have <?php echo $_GET['quantity'] ?> level <?php echo $_GET['level']?> characters in your party.</p>
+<p>OK, you have 
+    <?php echo $_GET['quantity'] ?> 
+    level <?php echo $_GET['level']?> 
+    character(s) in your party.</p>
 
 </body>
