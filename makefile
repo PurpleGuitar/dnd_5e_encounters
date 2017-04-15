c322741b54b01ab7719a3ee486d24a68d3372f92
Craig Oliver <craig.robert.oliver@gmail.com>
2017-04-15 16:28:12 -0400

    WIP Read XP threshold table

--- makefile
+++ makefile
@@ -1,3 +1,4 @@
 all: php/index.php
 	rm -rf deploy/*
 	cp php/*.php deploy
+	cp -r data deploy
--- php/index.php
+++ php/index.php
@@ -73,7 +73,7 @@ if (!isset($_GET['quantity']) || empty($_GET['quantity'])) {
 }
 ?>
 
-<h1>Encounter Report</h2>
+<h1>Encounter Reports</h2>
 
 <h2>Party Size</h2>
 
@@ -95,6 +95,38 @@ for ($i = 0; $i < count($quantity); $i++) {
 }
 ?>
 </ul>
+
+<h2>XP Budget</h2>
+
+<?php
+$xp_thresholds = array();
+if (($handle = fopen("data/xp_thresholds.csv", "r")) !== FALSE) {
+    $row = 0;
+    $col_names_to_numbers = array();
+    while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+        $row++;
+        $num_cols = count($data);
+        // Handle header row
+        if ($row == 1) {
+            for ($col = 0; $col < $num_cols; $col++) {
+                $name = $data[$col];
+                $col_names_to_numbers[$name] = $col;
+            }
+            continue;
+        } 
+        // Handle data row
+        for ($col = 0; $col < $num_cols; $col++) {
+            $table_row = array();
+            $table_row["Easy"] = $data[$col_names_to_numbers["Easy"]];
+            $table_row["Medium"] = $data[$col_names_to_numbers["Medium"]];
+            $table_row["Hard"] = $data[$col_names_to_numbers["Hard"]];
+            $table_row["Deadly"] = $data[$col_names_to_numbers["Deadly"]];
+            $xp_thresholds[$data[$col_names_to_numbers["Level"]]] = $table_row;
+        }
+    }
+    fclose($handle);
+}
+?>
 
 </body>
 </html>
