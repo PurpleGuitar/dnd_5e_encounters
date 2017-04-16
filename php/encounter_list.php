12a9316c3e5ec7f273ed04bb5ed82582cf136215
Craig Oliver <craig.robert.oliver@gmail.com>
2017-04-16 19:22:20 -0400

    Add encounter and reward XP to table

--- php/encounter_list.php
+++ php/encounter_list.php
@@ -8,9 +8,6 @@ require_once("cr_to_xp.php");
 
 <?php
 
-print_r($CR_TO_XP);
-print_r($CR_KEYS);
-
 $max_quantity = 20;
 $encounter_list = array();
 
@@ -26,37 +23,43 @@ while ($still_searching) {
     $current_cr_index = 0;
     while ($still_incrementing) {
         if (count($creatures) <= $current_cr_index) {
-            echo "before: ";
-            print_r($creatures);
-
             array_push($creatures, 0);
-            echo " After: ";
-            print_r($creatures);
-            echo "<br/>";
         }
         if ($creatures[$current_cr_index] < $max_quantity) {
             $creatures[$current_cr_index]++;
             $still_incrementing = false;
         } else {
+            $creatures[$current_cr_index] = 0;
             $current_cr_index++;
             if ($current_cr_index >= count($CR_KEYS)) {
                 $still_incrementing = false;
             }
         }
-
     }
-    print_r($creatures);
-    echo "<br/>";
 
+    // Create encounter
+    $encounter = array();
+    $encounter["Encounter"] = "";
+    $encounter["Reward XP"] = 0;
+    for ($i = 0; $i < count($creatures); $i++) {
+        $quantity = $creatures[$i];
+        if ($quantity > 0) {
+            $cr_name = $CR_KEYS[$i];
+            $xp_value = $CR_TO_XP[$cr_name];
+            if (strlen($encounter["Encounter"]) > 0) {
+                $encounter["Encounter"] .= "; ";
+            }
+            $encounter["Encounter"] .= $quantity . " " . CR . " " . $cr_name;
+            $encounter["Reward XP"] += $xp_value * $quantity;
+        }
+    }
+    array_push($encounter_list, $encounter);
 
-
-
-    // handbrake
-    if ($iterations >= 25) {
+    // Emergency brake
+    if ($iterations >= 5) {
         $still_searching = false;
     }
 }
-
 
 ?>
 
@@ -69,4 +72,16 @@ while ($still_searching) {
         <th>Encounter Size Multiplier</th>
         <th>Difficulty XP</th>
     </tr>
+<?php
+foreach ($encounter_list as $encounter) {
+    echo "<tr>";
+    echo "<td></td>";
+    echo "<td>".$encounter["Encounter"]."</td>";
+    echo "<td>".$encounter["Reward XP"]."</td>";
+    echo "<td></td>";
+    echo "<td></td>";
+    echo "<td></td>";
+    echo "</tr>";
+}
+?>
 </table>
